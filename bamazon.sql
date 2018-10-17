3e1ee8042f6bf6822885d357733918d272c70c8b
unknown <jayng168@gmail.com>
2018-10-17 19:28:29 -0400

    Starting with the JS file

---
+++ bamazon.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(20),
+department_name VARCHAR(20),
+price INTEGER(10),
+stock_quantity INTEGER(10),
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Toothbrush","Toiletries",3,500);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Bicycle","Sports & Outdoors",200,50);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Television","Electronics",1000,100);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Chair","Furniture",50,50);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Coffee Table","Furniture",65,30);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Sneakers","Sports & Outdoors",60,50);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Yogurt","Food",1,500);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Razor","Toiletries",10,150);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Book","Literature",20,500);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Laptop","Electronics",800,200);
+
+SELECT * FROM products;
